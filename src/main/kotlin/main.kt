afc12501eca0e980e5c7e633aa55c5095a30441b
Rainer Ramos <rainermr89@hotmail.com>
2021-10-13 15:46:16 -0300

    Restrições das variáveis

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,13 @@
 fun main(){
     println("Bem vindo ao Bytebank! :)")
-    val titular = "Alex"
+    val titular: String = "Alex"
+    val numeroConta: Int = 1000
+    var saldo: Double = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
 
     println("titular ${titular}")
+    println("número da conta ${numeroConta}")
+    println("saldo da conta ${saldo}")
 }
