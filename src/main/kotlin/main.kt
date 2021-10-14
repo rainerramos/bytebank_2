0094bad45e27e50f79fda9ba9ad35870d78b235d
Rainer Ramos <rainermr89@hotmail.com>
2021-10-14 09:00:50 -0300

    Criando classes e objetos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,35 @@
 fun main() {
     println("Bem vindo ao Bytebank! :)")
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+
+fun testaLacos() {
     var i = 0
-    while(i < 5){
+    while (i < 5) {
         val titular: String = "Alex $i"
         val numeroConta: Int = 1000 + i
         var saldo = i + 10.0
@@ -13,16 +41,13 @@ fun main() {
         i++
     }
 
-//    for (i in 10 downTo 1) {
-//
-//        if(i == 4) {
-//            break
-//        }
+    for (i in 10 downTo 1) {
 
-//    }
+        if (i == 4) {
+            break
+        }
 
-//    testaCondicoes(saldo)
-
+    }
 }
 
 fun testaCondicoes(saldo: Double) {
