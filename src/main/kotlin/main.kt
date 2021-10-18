af0aefdde0fe4372e51c44467a503c80a9ba31af
Rainer Ramos <rainermr89@hotmail.com>
2021-10-18 14:41:13 -0300

    Herdando de funcionário

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -18,12 +18,33 @@ fun main() {
         salario = 2000.0,
         senha = 1234
     )
+
     println("nome ${fran.nome}")
     println("cpf ${fran.cpf}")
     println("salario ${fran.salario}")
     println("bonificação ${fran.bonificacao()}")
 
-    if(fran.autentica(1234)){
+    if (fran.autentica(1234)) {
+        println("autenticou com sucesso")
+    } else {
+        println("falha na autenticação")
+    }
+
+    val gui = Diretor(
+        nome = "Gui",
+        cpf = "333.333.333-33",
+        salario = 4000.0,
+        senha = 4000,
+        plr = 200.0
+    )
+
+    println("nome ${gui.nome}")
+    println("cpf ${gui.cpf}")
+    println("salario ${gui.salario}")
+    println("bonificação ${gui.bonificacao()}")
+    println("plr ${gui.plr}")
+
+    if (fran.autentica(4000)) {
         println("autenticou com sucesso")
     } else {
         println("falha na autenticação")
