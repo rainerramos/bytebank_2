dd5e4194426b9c88599dae0aba7244ea7a7e1176
Rainer Ramos <rainermr89@hotmail.com>
2021-10-18 14:04:07 -0300

    Criando uma nova classe

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,15 @@
 fun main() {
     println("Bem vindo ao Bytebank! :)")
 
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 1000.00
+    )
+
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salario ${alex.salario}")
+    println("bonificação ${alex.bonificacao()}")
 }
 
