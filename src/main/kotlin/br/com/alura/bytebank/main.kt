3f72c1a7b321c161ed17f20b529429973db56940
Rainer Ramos <rainermr89@hotmail.com>
2021-10-27 19:48:47 -0300

    Utilizando variáveis globais

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,7 +1,17 @@
+import br.com.alura.bytebank.modelo.Cliente
+import br.com.alura.bytebank.modelo.ContaCorrente
+import br.com.alura.bytebank.modelo.ContaPoupanca
+import br.com.alura.bytebank.modelo.totalContas
+
+
 fun main() {
+    val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
+    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
+    val contaCorrente = ContaCorrente(titular = alex, numero = 1001)
+
     testaContasDiferentes()
 
-    //apenas notas sobre Herança, polimorfismo, interfarces e composição
+    println("Total de contas: ${totalContas}")
 }
 
 
