f9c9ff923231b4ec227947bab3bb5a8dc857e86a
Rainer Ramos <rainermr89@hotmail.com>
2021-10-29 21:18:08 -0300

    Utilizando o object declarations e companion object - parte 1

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -5,7 +5,7 @@ import br.com.alura.bytebank.modelo.*
 
 fun main() {
 
-    val fran = object : Autenticavel {
+    val fran = object  : Autenticavel {
         val nome: String = "Fran"
         val cpf: String = "111.111.111-11"
         val senha: Int = 1000
@@ -14,8 +14,11 @@ fun main() {
     }
     val sistemaInterno = SistemaInterno()
     sistemaInterno.entra(fran, 1000)
-
     println("nome do cliente ${fran.nome}")
+
+    object Contador {
+
+    }
 
     val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
     val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
