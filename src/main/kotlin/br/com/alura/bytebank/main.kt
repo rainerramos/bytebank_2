3570b1dbd717e025954bdaef0d2e888b16ab1807
Rainer Ramos <rainermr89@hotmail.com>
2021-11-02 20:33:47 -0300

    Introdução à pilha de execução

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,43 +1,27 @@
-import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.teste.testaAny
 
 fun main() {
-    println()
-    println(1)
-    println(1.0)
-    println(true)
-    println("teste")
-
-    val endereco = Endereco(
-        logradouro = "Rua vergueiro",
-        complemento = "Alura",
-        cep = "00000-0700"
-    )
-    val enderecoNovo = Endereco(
-        logradouro = "Rua vergueiro",
-        complemento = "Alura",
-        cep = "00000-0700"
-    )
-
-    println(endereco.equals(enderecoNovo))
-
-    println(endereco.hashCode())
-    println(enderecoNovo.hashCode())
-
-    println(endereco)
-    println(enderecoNovo)
-
-    println(
-        "${endereco.javaClass}@${
-            Integer.toHexString(endereco.hashCode())
-        }"
-    )
+    println("início main")
+    funcao1()
+    println("fim main")
 }
 
-fun imprime(valor: Any): Any {
-    println(valor)
-    return valor
+fun funcao1(){
+    println("início funcao1")
+    funcao2()
+    println("fim funcao1")
 }
+
+fun funcao2(){
+    println("início funcao2")
+    for (i in 1..5){
+        println(i)
+    }
+    println("fim funcao2")
+}
+
+
+
 
 
 
